3de3a74868b573c5eb59cf3728839f85e8dfe619
Michael Reed <myk27x@yahoo.com>
2015-09-25 19:58:45 -0400

    added activerecord and stories db class

--- ironnews.rb
+++ ironnews.rb
@@ -1,7 +1,19 @@
+require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: File.dirname(__FILE__) + "/ironnews.sqlite3"
+)
+
+class Stories < ActiveRecord::Base
+end
+
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'erb'
 set :port, 3027
+
+require 'erb'
 Tilt.register Tilt::ERBTemplate, 'html.erb'
 
 get '/' do
