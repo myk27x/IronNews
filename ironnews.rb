982d6834109a91b3803f059c3e6582ac8cd4d6bc
Michael Reed <myk27x@yahoo.com>
2015-09-27 21:53:10 -0400

    added email format checker and faux pagination

--- ironnews.rb
+++ ironnews.rb
@@ -7,6 +7,10 @@ ActiveRecord::Base.establish_connection(
 )
 
 class Stories < ActiveRecord::Base
+  email_format_checker = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :title, :link, :email, presence: true
+  # validates :link, format:
+  validates :email, format: email_format_checker
 end
 
 require 'sinatra'
@@ -18,6 +22,38 @@ Tilt.register Tilt::ERBTemplate, 'html.erb'
 
 get '/' do
   erb :home
+end
+
+get '/page2' do
+  erb :page2
+end
+
+get '/page3' do
+  erb :page3
+end
+
+get '/page4' do
+  erb :page4
+end
+
+get '/page5' do
+  erb :page5
+end
+
+get '/new' do
+  erb :new
+end
+
+get '/add' do
+    unless Stories.create(title: params["title"], link: params["link"], email: params["email"]).valid?
+      redirect "/new_error"
+    else
+      redirect "/"
+    end
+end
+
+get '/new_error' do
+  erb :new_error
 end
 
 get '/jobs' do
